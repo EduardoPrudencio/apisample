0e094a8139afdea3326f348ad382df8be9fdbb77
Eduardo Prudencio <eduardoprudencio@Eduardos-MacBook-Pro.local>
2024-02-24 11:01:03 -0300

    Crioando Dockerfile para criar o container do rss reaer

--- RssReaderContainer/Worker.cs
+++ RssReaderContainer/Worker.cs
@@ -17,23 +17,22 @@ namespace RssReaderContainer
 
             _reader = new Reader();
 
-            _list = _reader.Parse("https://www.tecmundo.com.br/rss");
+            _list = _reader.Parse("https://www.omnycontent.com/d/playlist/42233656-1562-49af-98d5-acd100df7932/a3504d75-e95a-41c5-8dda-aced013a0cb9/343561ea-888c-4641-bd55-aced013a0cd5/podcast.rss");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-
+                // ENFILEIRAR OBJETOS NO RABBITMQ        
                 foreach (var feed in _list)
                 {
-                    _logger.LogInformation("Título", feed.Title);
-                    _logger.LogInformation("Conteúdo", feed.Content);
-                    _logger.LogInformation("Data de publicação", feed.PublishDate);
+                    _logger.LogInformation($"Titulo {feed.Title}", feed.Title);
+                    _logger.LogInformation($"Conteudo {feed.Content}", feed.Content);
+                    _logger.LogInformation($"Data de publicao {feed.PublishDate}", feed.PublishDate);
                 }
 
-
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                _logger.LogInformation("A ista possui: {time} itens.", _list.Count());
                 await Task.Delay(1000, stoppingToken);
             }
         }
