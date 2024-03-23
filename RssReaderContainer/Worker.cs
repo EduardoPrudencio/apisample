52b4debaeed35d676f744e0720ba55b9d3a1fb9b
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2024-03-23 11:24:10 -0300

    Verificando se a mesnsagem já foi enfileirada antes de incluir na fila.

--- RssReaderContainer/Worker.cs
+++ RssReaderContainer/Worker.cs
@@ -13,6 +13,7 @@ namespace RssReaderContainer
         static Manager _queueManager;
         string fanoutName = "fanoutFeed";   
         string queueName = "omnycontent";
+        string[] feedIds;
 
         public Worker(ILogger<Worker> logger)
         {
@@ -21,6 +22,8 @@ namespace RssReaderContainer
             _reader = new Reader();
             _list = _reader.Parse("https://www.omnycontent.com/d/playlist/42233656-1562-49af-98d5-acd100df7932/a3504d75-e95a-41c5-8dda-aced013a0cb9/343561ea-888c-4641-bd55-aced013a0cd5/podcast.rss");
         
+            feedIds = new string[_list.Count()];
+
             _queueManager.CreateExchangeFanout(fanoutName, true, _queueManager.Connection);
             _queueManager.CreateQueue(queueName, _queueManager.Connection);
             _queueManager.BindingQueue(queueName, fanoutName, _queueManager.Connection, null);        
@@ -33,11 +36,18 @@ namespace RssReaderContainer
                 // ENFILEIRAR OBJETOS NO RABBITMQ        
                 JsonSerializerOptions jsonOptions = new JsonSerializerOptions {WriteIndented = true};
                 
+                int count = 0;
+
                 foreach (var feed in _list)
                 {
                     string feedJson = JsonSerializer.Serialize(feed, jsonOptions);
                     //_logger.LogInformation($"Feed: {feedJson}");
-                    _queueManager.Enqueue(feedJson, _queueManager.Connection, fanoutName);
+
+                    if (feed.Id != null && (Array.IndexOf(feedIds, feed.Id) < 0))
+                    {   _queueManager.Enqueue(feedJson, _queueManager.Connection, fanoutName); 
+                        feedIds[count] = feed.Id;
+                        count++;                     
+                    }
                 }
 
                // _logger.LogInformation("A ista possui: {time} itens.", _list.Count());
