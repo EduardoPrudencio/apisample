d8e218e56a1ad5c0dd1b1c2c69671594c8a80775
Eduardo Prudencio <eduardolobo.esp@gmail.com>
2024-05-26 13:01:00 -0300

    Alteração no Feed

--- ApiSample.Domain/Feed.cs
+++ ApiSample.Domain/Feed.cs
@@ -2,8 +2,8 @@
 {
     public struct Feed
     {
-        public string? Author { get; set; }
         public string? Id { get; set; }
+        public string? Author { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
         public string? Link { get; set; }
