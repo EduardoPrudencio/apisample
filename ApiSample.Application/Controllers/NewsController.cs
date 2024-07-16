41eec9835b88dcfc980ec1bf07b3dfeda4ea9d02
Eduardo Prudencio <eduardo.lobo@bemovin.com.br>
2024-07-16 17:14:46 -0300

    Referenciando Microsoft.EntityFrameworkCore

---
+++ ApiSample.Application/Controllers/NewsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiSample.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        // GET: api/<NewsController>
+        [HttpGet("id/{id}")]
+        public IEnumerable<string> Get(string id)
+        {
+             return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ ApiSample.Infraestrutura/MongoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiSample.Infraestrutura
+{
+    public class MongoDbContext: DbContext
+    {
+
+        public MongoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Customer>().ToCollection("customers");
+        }
+    }
+}
